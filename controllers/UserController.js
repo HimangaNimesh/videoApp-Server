e73a65be366f9047571c495a78255638b2ed5395
himanganimesh2 <himanganimesh2@gmail.com>
2023-06-09 00:01:01 +0530

    subscribe and unsubscribe is done

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -18,26 +18,72 @@ export const update = async(req,res, next) =>{
     }
 }
 
-export const deleting =(req,res, next) =>{
-    
+export const deleting =async(req,res, next) =>{
+    if(req.params.id === req.user.id) {
+        try {
+            await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("User has been deleted")
+        } catch (error) {
+            next(error)
+        }
+    }else{
+        return next(createError(400, "You can delete only your account"))
+    }
 }
 
-export const getUser =(req,res, next) =>{
-    
+export const getUser =async(req,res, next) =>{
+    try {
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user)
+    } catch (error) {
+        next(error)
+    }
 }
 
-export const subscribe =(req,res, next) =>{
-    
+export const subscribe =async(req,res, next) =>{
+    try {
+        await User.findById(req.user.id, {
+            $push: {subscribedUsers: req.params.id}
+        })
+        await User.findByIdAndUpdate(req.params.id, {
+            $inc: { subscribers: 1}
+        })
+        res.status(200).json("subscription successfull...")
+    } catch (error) {
+        next(error)
+    }
 }
 
-export const unsubscribe =(req,res, next) =>{
-    
+export const unsubscribe =async(req,res, next) =>{
+    try {
+        try {
+            await User.findById(req.user.id, {
+                $pull: {subscribedUsers: req.params.id}
+            })
+            await User.findByIdAndUpdate(req.params.id, {
+                $inc: { subscribers: -1}
+            })
+            res.status(200).json("unsubscription successfull...")
+        } catch (error) {
+            next(error)
+        } 
+    } catch (error) {
+        next(error)
+    }
 }
 
-export const like =(req,res, next) =>{
-    
+export const like =async(req,res, next) =>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
 }
 
-export const dislike =(req,res, next) =>{
-    
+export const dislike =async(req,res, next) =>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
 }
